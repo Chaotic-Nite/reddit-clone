1ee0840521b7f4f6cc1936ef095dd992fca19ea6
Richie1136 <Richiemajor1@gmail.com>
2021-07-07 09:54:13 -0400

    Noels changes made to the posts model

--- post_app/models.py
+++ post_app/models.py
@@ -1,18 +1,33 @@
 from django.db import models
+from user_app.models import RedditUser
 from django.utils import timezone
 
-# Create your models here.
 
-class Post(models.Model):
-  title = models.CharField(max_length=50)
-  content = models.TextField()
-  upvote = models.IntegerField()
-  downvote = models.IntegerField()
-  created_at = models.DateTimeField(default=timezone.now)
+# Constructed Mixin for both Post and Comment
+class CommonFieldsMixin(models.Model):
+    content = models.TextField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
+    created_by = models.ForeignKey(RedditUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
 
-  def __str__(self):
-    return self.title
+    def like_dislike(self):
+        return self.like - self.dislike
+
+    class Meta:
+        abstract = True
 
 
-  def votes(self):
-    return self.upvote-self.downvote
+class Comment(CommonFieldsMixin, models.Model):
+    ''' 
+        The common mixin contants all we'd need for comments so it just passes
+    '''
+    pass
+
+class Post(CommonFieldsMixin, models.Model):
+    ''' 
+        Special information for the Posts
+    '''
+    title = models.CharField(max_length=150)
+    url_post = models.URLField()
+    comments = models.ManyToManyField(Comment, symmetrical=False, blank=True)
