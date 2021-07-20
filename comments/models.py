85e764640bcb74f30f2ad7841a56bd11c520ea04
Richie1136 <Richiemajor1@gmail.com>
2021-07-20 11:44:37 -0400

    Trying to figure out how to comment on a comment

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 from post_app.models import CommonFieldsMixin, Post
+from django.utils import timezone
+
 
 class Comment(CommonFieldsMixin, MPTTModel):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
     body = models.TextField(max_length=4000)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
-
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    created_at = models.DateTimeField(default=timezone.now)
+  
     # class MPTTMeta:
         # order_insertion_by = ['-body']
         # level_attr = 'mptt_level'
